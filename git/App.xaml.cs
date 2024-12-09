243146b69c9028e81c775cc861756be950a7f442
LINEwoor <70282441+LINEwoor@users.noreply.github.com>
2024-12-09 22:07:29 +0700

    Добавьте файлы проекта.

---
+++ git/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace git
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
